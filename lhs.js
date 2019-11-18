3399efc05337e0f95a8f70267e3c0a6543acb48e
ezhq <ezhq@outlook.com>
2019-11-18 13:36:08 +0800

    Basic Info file design.

--- lhs.js
+++ lhs.js
@@ -1,3 +1,27 @@
 /* =============================== License =============================== */
-// Copyright (c) 2019, ezhq.
+//  Copyright © 2019 ezhq. All rights reserved.
 // https://github.com/seegear/lhs.git
+
+/* =============================== Env =============================== */
+const fs = require('fs')
+const request = require('syncrequest')
+const cheerio = require('cheerio')
+const log = console.log.bind(console)
+
+/* =============================== Bsic =============================== */
+class Hero {
+    constructor() {
+        this.nameEn = ''
+        this.avatarUrl = ''
+        this.infoUrl = ''
+    }
+}
+/* =============================== Main =============================== */
+const _main = () => {
+    log('===>LHS Start: ')
+    log('===>LHS End. Goodbye.')
+    log('')
+}
+
+/* =============================== Run =============================== */
+_main()
